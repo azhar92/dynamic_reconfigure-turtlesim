27f1dfb8952376fa4f82887742913e89abf5910e
Mohamed Azhar <a0110710@u.nus.edu>
2017-02-13 16:58:58 +0800

    Update client.py

--- nodes/client.py
+++ nodes/client.py
@@ -9,25 +9,78 @@ import dynamic_reconfigure.client
 from geometry_msgs.msg import Vector3, Twist
 
 def callback(config):
-    rospy.loginfo("Config set to {int_param}, {double_param}, {str_param}, {bool_param}, {size}".format(**config))
+	global pub,Zw,aw,bw,cw,dw,global_name,manual_name, parked_name, supervisory_name, autonomous_name, client
+	print config
+	global_name = config['bool_param']
+	manual_name = config['Manual']
+	parked_name = config['Parked']
+	supervisory_name = config['Supervisory']
+	autonomous_name = config['Autonomous']
+	#global_name = not global_name
+    #rospy.loginfo("Config set to {bool_param}".format(**config))
+ 
+
+
+pub = rospy.Publisher('/turtle1/cmd_vel', Twist)    
+aw = Twist(Vector3(1,0,0), Vector3(0,0,0))
+bw = Twist(Vector3(-1,0,0), Vector3(0,0,0)) 
+cw = Twist(Vector3(1,1,0), Vector3(0,0,1))
+dw = Twist(Vector3(1,0,0), Vector3(0,0,-1))
+Zw = Twist(Vector3(0,0,0), Vector3(0,0,0))
+
 
 if __name__ == "__main__":
-    rospy.init_node("dynamic_client")
-    pub = rospy.Publisher('/turtle1/cmd_vel', Twist)
-    rospy.wait_for_service("/dynamic_tutorials/set_parameters")
-    tw = Twist(Vector3(0.5,0,0), Vector3(0,0,0.5))
-    client = dynamic_reconfigure.client.Client("dynamic_tutorials", timeout=30, config_callback=callback)
+	rospy.init_node("dynamic_client")
+	rospy.wait_for_service("/dynamic_tutorials/set_parameters")
+	client = dynamic_reconfigure.client.Client("dynamic_tutorials", timeout=30, config_callback=callback)
+	r = rospy.Rate(1)
 
-    r = rospy.Rate(1)
-    x = 0
-    b = False
-    while not rospy.is_shutdown():
-        x = x+1
-        if x>5:
-            pub.publish(tw) 
-            #  print "rostopic pub /turtle1/cmd_vel geometry_msgs/Twist -r 1 -- '[2.0, 0.0, 0.0]' '[0.0, 0.0, 0.5]'"
-            x=0
+	global_name = False
+	manual_name = False
+	parked_name = False
+	supervisory_name = False
+	autonomous_name = False
+	
+	#global_name = rospy.get_param("/dynamic_tutorials/bool_param") 
+	#manual_name = rospy.get_param("/dynamic_tutorials/Manual")
+	#parked_name = rospy.get_param("/dynamic_tutorials/Parked")
+	#supervisory_name = rospy.get_param("/dynamic_tutorials/Supervisory")
+	#autonomous_name = rospy.get_param("/dynamic_tutorials/Autonomous")
+	
+	while not rospy.is_shutdown():	     	
+		if global_name == True: 
+			if manual_name == True and parked_name == True and supervisory_name == True and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == True and supervisory_name == True and autonomous_name == False:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == True and supervisory_name == False and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == True and supervisory_name == False and autonomous_name == False:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == False and supervisory_name == True and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == False and supervisory_name == True and autonomous_name == False:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == False and supervisory_name == False and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == True and parked_name == False and supervisory_name == False and autonomous_name == False:
+				pub.publish(aw)
+			elif manual_name == False and parked_name == True and supervisory_name == True and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == False and parked_name == True and supervisory_name == True and autonomous_name == False:
+				pub.publish(Zw)	
+			elif manual_name == False and parked_name == True and supervisory_name == False and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == False and parked_name == True and supervisory_name == False and autonomous_name == False:
+				pub.publish(bw)
+			elif manual_name == False and parked_name == False and supervisory_name == True and autonomous_name == True:
+				pub.publish(Zw)
+			elif manual_name == False and parked_name == False and supervisory_name == True and autonomous_name == False:
+				pub.publish(cw)
+			elif manual_name == False and parked_name == False and supervisory_name == False and autonomous_name == True:
+				pub.publish(dw)
+			elif manual_name == False and parked_name == False and supervisory_name == False and autonomous_name == False:
+				pub.publish(Zw)	
 
-#b = not b
-        client.update_configuration({"int_param":x, "double_param":(1/(x+1)), "str_param":str(rospy.get_rostime()), "bool_param":b, "size":1})
-        r.sleep()
+
+		r.sleep()
